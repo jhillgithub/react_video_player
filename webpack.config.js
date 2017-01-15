1c80d182943701ad27b096333b896ae0179bc9d2
jhillgithub <jhillgithub@users.noreply.github.com>
2017-01-15 12:26:22 -0600

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env': {
+            'youtube_api_key': process.env.youtube_api_key
+        }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
