a62acd6c41471e9b35d555332e5e87447ecce2ea
jhillgithub <jhillgithub@users.noreply.github.com>
2017-01-15 20:27:55 -0600

    removed es6 style function from server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-	console.log('server started on port: ', PORT);
-});
+// app.listen(PORT, () => {
+// 	console.log('server started on port: ', PORT);
+// });
+app.listen(process.env.PORT || 8080)
